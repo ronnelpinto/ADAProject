f2fa3f848a080b7d343a8560ffddf6ce781cea32
Ronnel Maclean Pinto <ronnel_pinto@intuit.com>
2021-10-25 00:32:50 +0800

    Update PrivateKey.java

--- PrivateKey.java
+++ PrivateKey.java
@@ -5,26 +5,24 @@ import java.math.BigInteger;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 public class PrivateKey implements java.io.Serializable{
     
-    private BigInteger d;
-    private BigInteger n;
+    private BigInteger mainKey;
+    private BigInteger subKey;
     
-    public void setd(BigInteger dValue){
-        this.d = dValue;
+    public void setd(BigInteger mainKeyValue){
+        this.mainKey = mainKeyValue;
     }
     
-    public void setn(BigInteger nValue){
-        this.n = nValue;
+    public void setn(BigInteger subKeyValue){
+        this.subKey = subKeyValue;
     }
     
     public BigInteger getd(){
-        return d;
+        return mainKey;
     }
     
     public BigInteger getn(){
-        return n;
+        return subKey;
     }
 }
